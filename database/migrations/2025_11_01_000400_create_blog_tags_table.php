b454381ff5f343fd765df636a86750d2ca9853f5
Marcelo <86026142+Joycemg@users.noreply.github.com>
2025-10-31 17:34:59 -0300

    Add missing blog post tag pivot migration

--- database/migrations/2025_11_01_000400_create_blog_tags_table.php
+++ database/migrations/2025_11_01_000400_create_blog_tags_table.php
@@ -15,27 +15,10 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('blog_post_blog_tag', function (Blueprint $table): void {
-            $table->unsignedBigInteger('blog_post_id');
-            $table->unsignedBigInteger('blog_tag_id');
-
-            $table->foreign('blog_post_id')
-                ->references('id')
-                ->on('blog_posts')
-                ->cascadeOnDelete();
-
-            $table->foreign('blog_tag_id')
-                ->references('id')
-                ->on('blog_tags')
-                ->cascadeOnDelete();
-
-            $table->primary(['blog_post_id', 'blog_tag_id']);
-        });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('blog_post_blog_tag');
         Schema::dropIfExists('blog_tags');
     }
 };
